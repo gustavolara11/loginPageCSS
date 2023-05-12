5589252bc469fa7016f3d5644d0ab0de7719ef36
gustavolara11 <gustavex2007@gmail.com>
2023-05-11 22:21:12 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const registerClick = () => {
+  let animation = document.getElementById("animation-box");
+  animation.style.marginLeft = "-20rem";
+};
+const loginClick = () => {
+  let animation = document.getElementById("animation-box");
+  animation.style.marginLeft = "30rem";
+};
