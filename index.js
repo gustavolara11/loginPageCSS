b59b980101e1882b5b9af78c443de4a32ac48af1
gustavolara11 <gustavex2007@gmail.com>
2023-05-13 22:17:59 -0300

    feat: add opacity transition on login and register

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const registerClick = () => {
   let animation = document.getElementById("animation-box");
   animation.style.marginLeft = "-20rem";
+  let login = document.getElementById("login");
+  let register = document.getElementById("register");
+  register.style.opacity = "1";
+  login.style.opacity = "0";
 };
 const loginClick = () => {
   let animation = document.getElementById("animation-box");
   animation.style.marginLeft = "30rem";
+  let login = document.getElementById("login");
+  let register = document.getElementById("register");
+  login.style.opacity = "1";
+  register.style.opacity = "0";
 };
